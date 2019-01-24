416a97140e355d6c79e052f83451b8ef8c271fdf
62442katieb <kbott006@fiu.edu>
2019-01-24 10:54:15 -0500

    checkpoint: finished methods & results draft on jan. 22, 2019

--- post-stats.py
+++ post-stats.py
@@ -3,7 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from os.path import join
-from scipy.stats import pearsonr, spearmanr, ttest_rel, ttest_ind, levene
+from scipy.stats import pearsonr, spearmanr, ttest_rel, ttest_ind, levene, mannwhitneyu
 import statsmodels.api as sm
 
 def jili_sidak_mc(data, alpha):
@@ -37,12 +37,16 @@ sns.set_style("whitegrid")
 
 data_dir = '/Users/Katie/Dropbox/Projects/physics-retrieval/data'
 df = pd.read_csv(join(data_dir, 'iq+brain+demo.csv'), index_col=0, header=0)
-f_gender_df = pd.read_csv(join(data_dir, 'rescored_gender_identity_female-post.csv'), index_col=0, header=0)
-m_gender_df = pd.read_csv(join(data_dir, 'rescored_gender_identity_male-post.csv'), index_col=0, header=0)
+f_gender_df = pd.read_csv(join(data_dir, 'rescored_gender_identity_female.csv'), index_col=0, header=[0,1])
+m_gender_df = pd.read_csv(join(data_dir, 'rescored_gender_identity_male.csv'), index_col=0, header=[0,1])
 phy_rt_df = pd.read_csv(join(data_dir, 'retr_physcond_accuracy_by_gender_post.txt'), sep='\t', index_col=1)
 gen_rt_df = pd.read_csv(join(data_dir, 'retr_gencond_accuracy_by_gender_post.txt'), sep='\t', index_col=1)
 
 df = pd.concat([df, phy_rt_df], axis=1, sort=True)
+#we don't care about pre data, only post
+f_gender_df.drop(['1'], axis=1, inplace=True)
+m_gender_df.drop(['1'], axis=1, inplace=True)
+
 
 mean = pd.Series(df.mean())
 median = pd.Series(df.median())
@@ -58,14 +62,22 @@ descriptives.to_csv(join(data_dir, 'decriptives.csv'))
 
 df_ladies = df[df['Sex'] == 'F']
 df_ladies = df_ladies.drop('Sex', axis=1)
+df_ladies = df_ladies.drop('Session', axis=1)
+df_ladies = df_ladies.drop('Gender', axis=1)
+df_ladies = df_ladies.drop('Class', axis=1)
+df_ladies = df_ladies.drop('Gender And Class', axis=1)
 df_ladies = pd.concat([df_ladies, f_gender_df], axis=1, sort=False)
 
 df_dudes = df[df['Sex'] == 'M']
 df_dudes = df_dudes.drop('Sex', axis=1)
+df_dudes = df_dudes.drop('Session', axis=1)
+df_dudes = df_dudes.drop('Gender', axis=1)
+df_dudes = df_dudes.drop('Gender And Class', axis=1)
+df_dudes = df_dudes.drop('Class', axis=1)
 df_dudes = pd.concat([df_dudes, m_gender_df], axis=1, sort=False)
 
-f_gender_df['Masculinity'] = 6 - f_gender_df['Total']
-m_gender_df['Masculinity'] = m_gender_df['Total']
+f_gender_df['Masculinity'] = 6 - f_gender_df['2', 'GID Post']
+m_gender_df['Masculinity'] = m_gender_df['2', 'GID Post']
 df_ladies['Masculinity'] = f_gender_df['Masculinity']
 df_dudes['Masculinity'] = m_gender_df['Masculinity']
 
@@ -114,7 +126,7 @@ brain = ['fc default mode-left central executive gen',
         'le right central executive gen', 'le right central executive phy']
 behav = ['Phy48Grade', 'Verbal Comprehension Sum_2','Perceptual Reasoning Sum_2', 'Full Scale IQ_2', 'Total', 'Mean Physics Retrieval Accuracy']
 
-#compare brain measures in physics and general conditions, separating male & female ppts
+#compare brain measures in physics and general conditions, then separate male & female ppts and repeat
 pairs = {}
 
 pairs['dmn_rcen'] = ttest_rel(df['fc default mode-right central executive gen'], df['fc default mode-right central executive phy'])
@@ -168,23 +180,63 @@ paired_ttests.to_csv(join(data_dir, 'paired-ttests_brain.csv'))
 
 #now, compare male and female ppts on each brain measure
 sex_diff = {}
-for key in df_ladies.keys():
-    unequal_var = levene(df_ladies[key], df_dudes[key], center='mean')
-    if unequal_var[1] < 0.05:
-        sex_diff[key] = ttest_ind(df_ladies[key], df_dudes[key], equal_var=True)
-    else:
-        sex_diff[key] = ttest_ind(df_ladies[key], df_dudes[key], equal_var=False)
+#for key in df_ladies.keys():
+#    unequal_var = levene(df_ladies[key], df_dudes[key], center='mean')
+#    if unequal_var[1] < 0.05:
+#        sex_diff[key] = ttest_ind(df_ladies[key], df_dudes[key], equal_var=True)
+#    else:
+#        sex_diff[key] = ttest_ind(df_ladies[key], df_dudes[key], equal_var=False)
+#should actually be using mannwhitneyu instead of ttest_ind
+keys = ['AgeOnScanDate','Phy48Grade','GPA.PreSem',
+        'fc default mode-left central executive gen',
+                  'fc default mode-left central executive phy',
+                 'fc default mode-right central executive gen',
+                 'fc default mode-right central executive phy',
+                                   'fc hippo-default mode gen',
+                                   'fc hippo-default mode phy',
+                         'fc hippo-left central executive gen',
+                         'fc hippo-left central executive phy',
+                        'fc hippo-right central executive gen',
+                        'fc hippo-right central executive phy',
+       'fc left central executive-right central executive gen',
+       'fc left central executive-right central executive phy',
+                                       'global efficiency gen',
+                                       'global efficiency phy',
+                                         'le default mode gen',
+                                         'le default mode phy',
+                               'le left central executive gen',
+                               'le left central executive phy',
+                              'le right central executive gen',
+                              'le right central executive phy',
+                                  'Verbal Comprehension Sum_2',
+                                  'Perceptual Reasoning Sum_2',
+                                             'Full Scale IQ_2',
+                                                       'index',
+                             'Mean Physics Retrieval Accuracy',
+                                             'Mean Correct RT',
+                                           'Mean Incorrect RT',
+                                             ('2', 'GID Post'),
+                                                 'Masculinity']
+
+for key in keys:
+    sex_diff[key] = mannwhitneyu(df_ladies[key], df_dudes[key])
 
 sex_differences = pd.DataFrame.from_dict(sex_diff, orient='index')
 sex_differences.to_csv(join(data_dir, 'sex_differences.csv'))
 
 all_gend = pd.concat([f_gender_df, m_gender_df], axis=0, sort=True)
 big_df = pd.concat([df, all_gend], axis=1, sort=True)
+big_df['Female'] = big_df['Sex']
+big_df.replace({'Female': {'M': 1, 'F': 2}}, inplace=True)
+big_df['sexXIQ'] = big_df['Full Scale IQ_2'] * big_df['Female']
+big_df['sexXgender'] = big_df['Total'] * big_df['Female']
+big_df['sexXmasc'] = big_df['Masculinity'] * big_df['Female']
+big_df['const'] = 1
 
 behav = ['Phy48Grade', 'Verbal Comprehension Sum_2',
          'Perceptual Reasoning Sum_2', 'Full Scale IQ_2', 'Total', 'Mean Physics Retrieval Accuracy', 'Masculinity']
 
-jili_sidak_mc(big_df[behav],0.05)
+jili_sidak_mc(big_df[brain],0.05)
 jili_sidak_mc(df_dudes[behav],0.05)
 jili_sidak_mc(df_ladies[behav],0.05)
 
@@ -234,3 +286,105 @@ m_gid_behav_corr = pd.DataFrame.from_dict(m_gid_behav, orient='index')
 m_gid_behav_corr.to_csv(join(data_dir, 'corr_male_gid_meas.csv'))
 gid_behav_corr = pd.DataFrame.from_dict(gid_behav, orient='index')
 gid_behav_corr.to_csv(join(data_dir, 'corr_gid_meas.csv'))
+
+########Now for some regressions!########
+
+conn = ['fc default mode-left central executive gen',
+       'fc default mode-left central executive phy',
+       'fc default mode-right central executive gen',
+       'fc default mode-right central executive phy',
+       'fc hippo-default mode gen', 'fc hippo-default mode phy',
+       'fc hippo-left central executive gen',
+       'fc hippo-left central executive phy',
+       'fc hippo-right central executive gen',
+       'fc hippo-right central executive phy',
+       'fc left central executive-right central executive gen',
+       'fc left central executive-right central executive phy']
+eff = ['global efficiency gen', 'global efficiency phy', 'le default mode gen',
+       'le default mode phy', 'le left central executive gen',
+       'le left central executive phy', 'le right central executive gen',
+       'le right central executive phy']
+
+demo_iq = ['Female', 'AgeOnScanDate', 'Full Scale IQ_2']
+academic = ['Female', 'Phy48Grade', 'SexXGrade']
+sex_gend = ['Female', 'Masculinity', 'sexXmasc']
+non_brain = ['Female', 'Verbal Comprehension Sum_2', 'Perceptual Reasoning Sum_2',
+             'Full Scale IQ_2', 'sexXIQ', 'Phy48Grade', 'SexXGrade', 'AgeOnScanDate']
+
+#does IQ predict brain connectivity, controlling for age and sex?
+iq_summary = pd.DataFrame(columns=['BIC', 'F', 'p(F)', 'df_model', 'R^2'], index=brain)
+iq_pvals = {}
+iq_params = {}
+
+for brain_var in brain:
+    #print('******************************{0}********************************'.format(brain_var))
+    y = big_df.loc[:, brain_var].dropna()
+    X = big_df.loc[:, demo_iq].dropna()
+    X = sm.add_constant(X)
+    model = sm.OLS(y, X).fit()
+    #print(model.summary())
+    iq_summary.at[brain_var,'BIC'] = model.bic
+    iq_summary.at[brain_var,'F'] = model.fvalue
+    iq_summary.at[brain_var,'df_model'] = model.df_model
+    iq_summary.at[brain_var,'p(F)'] = model.f_pvalue
+    iq_summary.at[brain_var,'R^2'] = model.rsquared
+
+    iq_params[brain_var] = model.params
+    iq_pvals[brain_var] = model.pvalues
+iq_summary.min()
+
+#what about sex and gender?
+sg_summary = pd.DataFrame(columns=['BIC', 'F', 'p(F)', 'df_model', 'R^2'], index=brain)
+sg_pvals = {}
+sg_params = {}
+
+no_missing_gender = big_df
+no_missing_gender.drop(335, axis=0, inplace=True)
+no_missing_gender.drop(344, axis=0, inplace=True)
+no_missing_gender.drop(465, axis=0, inplace=True)
+no_missing_gender.drop(587, axis=0, inplace=True)
+
+for brain_var in brain:
+    #print('******************************{0}********************************'.format(brain_var))
+    y = no_missing_gender.loc[:, brain_var].dropna()
+    X = no_missing_gender.loc[:, sex_gend].dropna()
+    X = sm.add_constant(X)
+    model = sm.OLS(y, X).fit()
+    #res = model.resid # residuals
+    #fig = sm.qqplot(res)
+    #plt.show()
+    #print(model.summary())
+    sg_summary.at[brain_var,'BIC'] = model.bic
+    sg_summary.at[brain_var,'F'] = model.fvalue
+    sg_summary.at[brain_var,'df_model'] = model.df_model
+    sg_summary.at[brain_var,'p(F)'] = model.f_pvalue
+    sg_summary.at[brain_var,'R^2'] = model.rsquared
+
+    sg_params[brain_var] = model.params
+    sg_pvals[brain_var] = model.pvalues
+sg_pvals
+
+#how do brain + iq + gender predict academic performance
+ac_summary = pd.DataFrame(columns=['BIC', 'F', 'p(F)', 'df_model', 'R^2'], index=brain)
+ac_pvals = {}
+ac_params = {}
+
+
+for brain_var in brain:
+    vars = list(set(demo_iq)) + [brain_var]
+    #vars += [brain_var]
+    #print('******************************{0}********************************'.format(brain_var))
+    y = big_df.loc[:, 'Phy48Grade'].dropna()
+    X = big_df.loc[:, vars].dropna()
+    X = sm.add_constant(X)
+    model = sm.OLS(y, X).fit()
+    #print(model.summary())
+    ac_summary.at[brain_var,'BIC'] = model.bic
+    ac_summary.at[brain_var,'F'] = model.fvalue
+    ac_summary.at[brain_var,'df_model'] = model.df_model
+    ac_summary.at[brain_var,'p(F)'] = model.f_pvalue
+    ac_summary.at[brain_var,'R^2'] = model.rsquared
+
+    ac_params[brain_var] = model.params
+    ac_pvals[brain_var] = model.pvalues
+ac_summary.min()
