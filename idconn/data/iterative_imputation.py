026f965c81454450530634d434a532875af0b6f8
Katie Bottenhorn <kbott006@fiu.edu>
2021-06-09 12:30:37 -0400

    fix typo in imputer

--- idconn/data/iterative_imputation.py
+++ idconn/data/iterative_imputation.py
@@ -19,7 +19,7 @@ from sklearn.impute import IterativeImputer
 def impute(data, max_iter):
     non_numeric = data.select_dtypes(exclude=['number']).columns
     dumb = pd.get_dummies(data[non_numeric], prefix='dummy')
-    df = pd.concat([data.drop(non_numeric, axis=1), dumb)
+    df = pd.concat([data.drop(non_numeric, axis=1), dumb])
     impute_pls = IterativeImputer(
         max_iter=10000, skip_complete=True, verbose=1, tol=5e-3, n_nearest_features=1000
     )
