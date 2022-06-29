add space to fname, fix tau computation

idconn/connectivity/build_networks.py
@@ -113,7 +113,7 @@ def task_connectivity(layout, subject, session, task, atlas, confounds, connecti
         ex_bold = image.index_img(bold_file[0], 2)
         display = plotting.plot_epi(ex_bold)
         display.add_contours(atlas)
-        display.savefig(join(deriv_dir,  f'sub-{subject}', f'ses-{session}', 'func', f'sub-{subject}_ses-{session}_task-{task}_run-{run}_desc-{atlas_name}_overlay.png'))
+        display.savefig(join(deriv_dir,  f'sub-{subject}', f'ses-{session}', 'func', f'sub-{subject}_ses-{session}_task-{task}_run-{run}_space-MNI152NLin2009cAsym_space-{atlas_name}_overlay.png'))
             
         print(f'BOLD file located at {bold_file}\nTR = {tr}s')
         
@@ -156,7 +156,7 @@ def task_connectivity(layout, subject, session, task, atlas, confounds, connecti
         corrmat_df = pd.DataFrame(index=np.arange(1, avg_corrmat.shape[0]+1), columns=np.arange(1, avg_corrmat.shape[0]+1),data=avg_corrmat)
         avg_corrmats[condition] = corrmat_df
         corrmat_file = join(deriv_dir,  
-                            f'sub-{subject}', f'ses-{session}', 'func', f'sub-{subject}_ses-{session}_task-{task}_condition-{condition}_desc-{atlas_name}_corrmat.tsv')
+                            f'sub-{subject}', f'ses-{session}', 'func', f'sub-{subject}_ses-{session}_task-{task}_desc-{condition}_space-MNI152NLin2009cAsym_atlas-{atlas_name}_corrmat.tsv')
         try:
             corrmat_df.to_csv(corrmat_file, sep='\t')
             files.append(corrmat_file)